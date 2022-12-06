47b7dcd28ba83314032005221ba80c7565c490d4
nikkimartin9555 <96130076+nikkimartin9555@users.noreply.github.com>
2022-12-06 13:35:57 -0500

    damage

--- Year-2-P2/Assets/Scripts/Damage.cs
+++ Year-2-P2/Assets/Scripts/Damage.cs
@@ -15,12 +15,18 @@ public class Damage : MonoBehaviour
 
     // Update is called once per frame
     void Update()
+
     {
         
+
     }
 
     void OnTriggerEnter(Collider other)
     {
-       
+        if (other.tag == "Player")
+        {
+            
+        }
+
     }
 }
