2fdfa7f75c91ddb78d0e51731d314770740623f1
Theodore Winterbottom <100698585+Theodore-Winterbottom@users.noreply.github.com>
2022-12-07 13:40:55 -0500

    Worked on Camera

--- SquirrelKnightAdventures Demo/Assets/Scripts/CameraMovementScript.cs
+++ SquirrelKnightAdventures Demo/Assets/Scripts/CameraMovementScript.cs
@@ -4,15 +4,39 @@ using UnityEngine;
 
 public class CameraMovementScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] private new Rigidbody camera;
+    [SerializeField] private float cameraMoveSpeed;
 
-    // Update is called once per frame
-    void Update()
+    private bool cameraIsPanning;
+
+    private void OnTriggerEnter(Collider other)
     {
-        
+        if (other.name == "left_barrier" || other.name == "right_barrier" || other.name == "middle_barrier")
+        {
+            calculateMovePos(other);
+        }
+    }
+    private void calculateMovePos(Collider other)
+    {
+        if (other.name != "middle_barrier" && !cameraIsPanning)
+        {
+            cameraIsPanning = true;
+
+            float barrierPoxX = other.transform.position.x;
+
+            float cameraPosX = camera.transform.position.x;
+
+            float posDifference = barrierPoxX - cameraPosX;
+
+            Vector3 moveDirection = new Vector3(posDifference, camera.transform.position.y, camera.transform.position.z);
+
+            camera.AddForce(moveDirection * cameraMoveSpeed);
+        } 
+
+        if(other.name == "middle_barrier")
+        {
+            camera.velocity = Vector3.zero;
+            cameraIsPanning = false;
+        }
     }
 }
