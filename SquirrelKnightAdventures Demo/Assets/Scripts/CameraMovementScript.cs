c94ba91cb95225ff1b6d296647ee9a2d8f1141df
Theodore Winterbottom <100698585+Theodore-Winterbottom@users.noreply.github.com>
2022-12-09 13:40:05 -0500

    Worked On Camera

--- SquirrelKnightAdventures Demo/Assets/Scripts/CameraMovementScript.cs
+++ SquirrelKnightAdventures Demo/Assets/Scripts/CameraMovementScript.cs
@@ -15,26 +15,29 @@ public class CameraMovementScript : MonoBehaviour
         float cameraPosX = camera.transform.position.x;
         PlayerPosX = transform.position.x;
         positionDifference = PlayerPosX - cameraPosX;
-        Debug.Log(positionDifference);
+        
+
         if (Mathf.Abs(positionDifference) >= 18 && !cameraIsPanning)
         {
             cameraIsPanning = true;
             calculateMovePos();
         }
+        if(cameraIsPanning)
+        {
+            calculateMovePos();
+        }
     }
     private void calculateMovePos()
     {
-        for (int i = 0; Mathf.Abs(positionDifference > 0); i++)
+        Debug.Log(positionDifference);
+
+        Vector3 moveDirection = new Vector3(positionDifference, camera.transform.position.y, camera.transform.position.z);
+
+        camera.velocity = ((moveDirection / 10) * (Mathf.Abs(positionDifference)/10) * cameraSpeedMultiplier * Time.deltaTime);
+        //camera.AddForce(moveDirection * Mathf.Abs(posDifference) * cameraSpeedMultiplier);
+        if (Mathf.Abs(positionDifference) < 5)
         {
-
-            Vector3 moveDirection = new Vector3(positionDifference, camera.transform.position.y, camera.transform.position.z);
-
-            camera.velocity = (moveDirection * Mathf.Abs(positionDifference) * cameraSpeedMultiplier * Time.deltaTime);
-            //camera.AddForce(moveDirection * Mathf.Abs(posDifference) * cameraSpeedMultiplier);
-            if (Mathf.Abs(positionDifference) < 5)
-            {
-                StopCamera();
-            }
+            StopCamera();
         }
     }
         
