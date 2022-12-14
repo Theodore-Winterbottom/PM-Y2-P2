151726fb12fc73eb1c0c39335f01098a8c5d3e00
Theodore Winterbottom <100698585+Theodore-Winterbottom@users.noreply.github.com>
2022-12-14 12:51:16 -0500

    worked on UI

---
+++ SquirrelKnightAdventures Demo/Assets/Scripts/UIUXScript.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIUXScript : MonoBehaviour
+{
+    [SerializeField] private GameObject PauseMenu;
+
+    [SerializeField] private GameObject SettingsMenu;
+
+    private bool GameIsPaused;
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (GameIsPaused)
+            {
+                Resume();
+            } 
+            else
+            {
+                LoadPauseMenu();
+            }     
+        }
+        if (Input.GetKey(KeyCode.R))
+        {
+            if (!GameIsPaused)
+            {
+                RestartGame();
+            }
+        }
+    }
+    public void LoadPauseMenu()
+    {
+        GameIsPaused = true;
+        Time.timeScale = 0f;
+        PauseMenu.SetActive(true);
+        SettingsMenu.SetActive(false);
+    }
+    public void Resume()
+    {
+        GameIsPaused = false;
+        Time.timeScale = 1f;
+        PauseMenu.SetActive(false);
+        SettingsMenu.SetActive(false);
+    }
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        Resume();
+    }
+    public void LoadSettings()
+    {
+        PauseMenu.SetActive(false);
+        SettingsMenu.SetActive(true);
+    }
+    public void Quit()
+    {
+        Time.timeScale = 1f;
+        Application.Quit();
+    }
+}
