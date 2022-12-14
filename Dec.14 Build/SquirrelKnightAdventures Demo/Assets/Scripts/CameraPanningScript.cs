992718336be5939499e5e53ec2741a98f4e26c9d
Theodore Winterbottom <100698585+Theodore-Winterbottom@users.noreply.github.com>
2022-12-14 12:02:50 -0500

    Added Latest Build

---
+++ Dec.14 Build/SquirrelKnightAdventures Demo/Assets/Scripts/CameraPanningScript.cs
@@ -0,0 +1,114 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEditor;
+using UnityEngine;
+
+#region Main Script
+public class CameraPanningScript : MonoBehaviour
+{
+    [SerializeField] Transform target_object;
+
+    [SerializeField] float distance_from_camera_cutoff;
+
+    [SerializeField] float distance_from_camera;
+
+    [SerializeField] float camera_speed_multiplier;
+
+    [SerializeField] float distance_from_center_cutoff;
+
+    private bool InRange;
+    private void FixedUpdate()
+    {
+        calculateDistance();
+    }
+    private void calculateDistance ()
+    {
+        distance_from_camera = target_object.position.x - GetComponent<Transform>().position.x;
+
+        if (Mathf.Abs(distance_from_camera) <= distance_from_center_cutoff)
+        {
+            GetComponent<Rigidbody>().velocity = Vector3.zero;
+        }
+
+        if (Mathf.Abs(distance_from_camera) > distance_from_camera_cutoff)
+        {
+            InRange = true;
+        }
+        else 
+        {
+            InRange = false;
+        }
+
+        if (InRange)
+        {
+            panCamera();
+        }
+    }
+    private void panCamera()
+    {
+        Vector3 move_direction = new Vector3(distance_from_camera, transform.position.y, transform.position.z);
+
+        GetComponent<Rigidbody>().velocity = (move_direction * camera_speed_multiplier);
+    }
+}
+#endregion
+
+#region Editor Region
+[CustomEditor(typeof(CameraPanningScript))]
+public class CameraEditor : Editor
+{
+    SerializedProperty target_object;
+    SerializedProperty distance_from_camera_cutoff;
+    SerializedProperty distance_from_camera;
+    SerializedProperty camera_speed_multiplier;
+    SerializedProperty distance_from_center_cutoff;
+    void OnEnable()
+    {
+        target_object = serializedObject.FindProperty("target_object");
+        distance_from_camera_cutoff = serializedObject.FindProperty("distance_from_camera_cutoff");
+        distance_from_camera = serializedObject.FindProperty("distance_from_camera");
+        camera_speed_multiplier = serializedObject.FindProperty("camera_speed_multiplier");
+        distance_from_center_cutoff = serializedObject.FindProperty("distance_from_center_cutoff");
+    }
+    public override void OnInspectorGUI()
+    {
+        serializedObject.Update();
+
+        EditorGUI.indentLevel = 10;
+
+        EditorGUILayout.Space(5f);
+
+        EditorGUILayout.LabelField("Camera Panning Script", EditorStyles.boldLabel);
+        
+        EditorGUI.indentLevel = 0;
+
+        EditorGUILayout.Space(5f);
+        EditorGUILayout.LabelField("Target Object", EditorStyles.boldLabel);
+        EditorGUILayout.Space(7f);
+
+        EditorGUILayout.ObjectField(target_object, GUIContent.none);
+        EditorGUILayout.Space(15f);
+        EditorGUILayout.LabelField("Distance From Camera Cutoff"
+            + "                                   Current Distance: " +
+            (Mathf.Abs(Mathf.Round(distance_from_camera.floatValue * 100) / 100)).ToString(), EditorStyles.boldLabel);
+
+        EditorGUILayout.Space(7f);
+        EditorGUILayout.Slider(distance_from_camera_cutoff, 0, 20, GUIContent.none);
+
+        EditorGUILayout.Space(15f);
+        EditorGUILayout.LabelField("Camera Speed Multiplier", EditorStyles.boldLabel);
+        EditorGUILayout.Space(7f);
+        EditorGUILayout.Slider(camera_speed_multiplier, 0, 2, GUIContent.none);
+
+        EditorGUILayout.Space(15f);
+        EditorGUILayout.LabelField("Distance From Center Cutoff", EditorStyles.boldLabel);
+        EditorGUILayout.Space(7f);
+        EditorGUILayout.Slider(distance_from_center_cutoff, 0, 2, GUIContent.none);
+
+        EditorGUILayout.Space(5f);
+
+        serializedObject.ApplyModifiedProperties();
+    }
+} 
+#endregion
---
+++ Dec.14 Build/SquirrelKnightAdventures Demo/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement: MonoBehaviour
+{
+    private Rigidbody PlayerRigidbody;
+    [Header("Movement Customization")]
+    [Space]
+    [SerializeField] private float playerSpeedMultiplier;
+    [SerializeField] private float jumpForceMultiplier;
+    [SerializeField] private bool PlayerIsGrounded;
+
+    private void Start ()
+    {
+        PlayerRigidbody = GetComponent<Rigidbody>();
+    }
+    private void FixedUpdate ()
+    {
+        calculateMovement();
+
+        if (Input.GetKey(KeyCode.Space) && PlayerIsGrounded)
+        {
+            calculateJump();
+        }
+    }
+    private void calculateMovement ()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+
+        Vector3 direction = new Vector3(horizontalInput, 0f, 0f);
+
+        transform.Translate(direction * playerSpeedMultiplier * Time.deltaTime);
+    }
+    private void calculateJump ()
+    {
+        PlayerRigidbody.AddForce(Vector3.up * jumpForceMultiplier);
+    }
+    void OnCollisionStay(UnityEngine.Collision collision)
+    {
+        if (collision.collider.tag == "Ground")
+        {
+            PlayerIsGrounded = true;
+        }
+    }
+    void OnCollisionExit(UnityEngine.Collision collision)
+    {
+        if (collision.collider.tag == "Ground")
+        {
+            PlayerIsGrounded = false;
+        }
+    }
+}
