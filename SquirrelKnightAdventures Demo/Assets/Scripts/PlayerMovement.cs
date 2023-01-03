730360f6b6e302bc7bbcffeb6516bdda8671dccd
Theodore Winterbottom <100698585+Theodore-Winterbottom@users.noreply.github.com>
2023-01-03 01:07:00 -0500

    qdqwdqdqdwq

--- SquirrelKnightAdventures Demo/Assets/Scripts/PlayerMovement.cs
+++ SquirrelKnightAdventures Demo/Assets/Scripts/PlayerMovement.cs
@@ -13,31 +13,39 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private Vector3 GroundBoxRadius;
     [SerializeField] private Vector3 ObsicalBoxRadius;
     [SerializeField] private float distanceToGroundOffset;
-    public RaycastHit hit;
+    private Vector3 wallOffset;
 
     private void Start()
     {
         playerRigidbody = GetComponent<Rigidbody>();
-        
     }
     
     private void FixedUpdate()
     {
         CalculateMovement();
 
-        if (Input.GetKey(KeyCode.Space) && CheckIsGrounded())
+        if (CheckIsGrounded() && Input.GetKey(KeyCode.Space))
         {
             CalculateJump();
         }
     }
+
     public bool CheckIsGrounded()
     {
         Vector3 center = GetComponent<Collider>().bounds.center;
-        RaycastHit hit1;
-        return Physics.BoxCast(GroundBoxRadius + center, transform.lossyScale /2, -transform.up, out hit, transform.rotation,
-            1, layermask, QueryTriggerInteraction.UseGlobal);
 
+        RaycastHit hit;
+        bool groundCheck =  Physics.BoxCast(GroundBoxRadius + center + wallOffset, transform.lossyScale /2 - new Vector3(.01f,0,.01f), 
+            -transform.up, out hit, transform.rotation, 1, layermask, QueryTriggerInteraction.UseGlobal);
+        Debug.Log(transform.lossyScale / 2 - new Vector3(.1f, 0, .1f));
+        if (groundCheck)
+        {
+            playerRigidbody.velocity = playerRigidbody.velocity.RemoveComponent(Physics.gravity);
+        }
+
+        return groundCheck;
     }
+
     private void CalculateMovement()
     {
         float horizontalInput = Input.GetAxis("Horizontal");
@@ -46,32 +54,46 @@ public class PlayerMovement : MonoBehaviour
 
         if (!CheckIfObstacles(direction))
         {
+            
             transform.Translate(direction * playerSpeedMultiplier * Time.deltaTime);
         }
     }
+
     public bool CheckIfObstacles(Vector3 moveDirection)
     {
+        wallOffset = Vector3.zero;
+
+        if (moveDirection.x > 0) { moveDirection.x = 1f; wallOffset.x = -.2f;}
+        if (moveDirection.x < 0) { moveDirection.x = -1f; wallOffset.x = .2f;}
+
         Vector3 center = GetComponent<Collider>().bounds.center;
-        if (moveDirection.x > 0) { moveDirection.x = 1f; }
-        if (moveDirection.x < 0) { moveDirection.x = -1f; }
+
         RaycastHit hit;
-        bool checkobsical = Physics.BoxCast(center, ObsicalBoxRadius + transform.lossyScale /2, moveDirection, out hit, transform.rotation,
+        bool obstacleCheck = Physics.BoxCast(center, ObsicalBoxRadius + transform.lossyScale /2, moveDirection, out hit, transform.rotation,
             1, layermask, QueryTriggerInteraction.UseGlobal);
-        return checkobsical;
+
+        if (obstacleCheck)
+        {
+            
+            
+            playerRigidbody.velocity = playerRigidbody.velocity.RemoveComponent(moveDirection);
+        }
+
+        return obstacleCheck;
     }
 
     private void CalculateJump()
     {
         playerRigidbody.AddForce(Vector3.up * jumpForceMultiplier, ForceMode.Impulse);
     }
-    private void OnDrawGizmosSelected()
+}
+
+public static class VectorExtensions
+{
+    public static Vector3 RemoveComponent(this Vector3 vector, Vector3 direction)
     {
-        float horizontalInput = Input.GetAxis("Horizontal");
-        Vector3 center = GetComponent<Collider>().bounds.center;
-        Vector3 direction = new(horizontalInput, 0, 0);
-        if (direction.x > 0) { direction.x = 1f; }
-        if (direction.x < 0) { direction.x = -1f; }
-        Gizmos.color = Color.red;
-        Gizmos.DrawWireCube(center + direction * hit.distance, ObsicalBoxRadius + transform.lossyScale);
+        direction = direction.normalized;
+
+        return vector - direction * Vector3.Dot(vector, direction);
     }
 }
