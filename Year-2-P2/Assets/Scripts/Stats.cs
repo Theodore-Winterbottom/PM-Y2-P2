17bc7dd3cb5dcae1caee31879934741d308f1f1e
nikkimartin9555 <96130076+nikkimartin9555@users.noreply.github.com>
2023-01-11 13:06:46 -0500

    timer version 2

--- Year-2-P2/Assets/Scripts/Stats.cs
+++ Year-2-P2/Assets/Scripts/Stats.cs
@@ -5,13 +5,13 @@ using Unity.VisualScripting;
 
 public class Stats : MonoBehaviour
 {
-    private float timerDuration = 0.01f * 60f;
+    // Set Time
+    private float timerDuration = 0.0001f * 60f;
 
     [SerializeField]
-    private bool countDown = true;
+    private bool countUp = true;
 
     private float timer;
-
     
     [SerializeField]
     private TextMeshProUGUI firstMinute;
@@ -24,40 +24,24 @@ public class Stats : MonoBehaviour
     [SerializeField]
     private TextMeshProUGUI secondSecond;
 
-    private float flashTimer;
-    private float flashDuration = 1f;
-
     // Start is called before the first frame update
     void Start()
     {
         ResetTimer();
     }
-
     // Update is called once per frame
     void Update()
     {
-        
-        if(countDown && timer > 0)
-        {
-            //Adds time
-            timer += Time.deltaTime;
-            UpdateTimerDisplay(timer);
-        }
-        /*
-        else if (!countDown && timer < timerDuration)
+        //Adds time
+        if (countUp && timer > 0)
         {
             timer += Time.deltaTime;
             UpdateTimerDisplay(timer);
         }
-        else
-        {
-            Flash();
-        }*/
     }
-
     private void ResetTimer()
     {
-        if(countDown)
+        if(countUp)
         {
             timer = timerDuration;
         }
@@ -65,11 +49,7 @@ public class Stats : MonoBehaviour
         {
             timer = 0;
         }
-        SetTextDisplay(true);
-
-        timer = timerDuration;
     }
-
     private void UpdateTimerDisplay(float time)
     {
         float minutes = Mathf.FloorToInt(time / 60);
@@ -80,44 +60,5 @@ public class Stats : MonoBehaviour
         secondMinute.text = currentTime[1].ToString();
         firstSecond.text = currentTime[2].ToString();
         secondSecond.text = currentTime[3].ToString();
-    }
-
-    private void Flash()
-    {
-        if(countDown && timer != 0)
-        {
-            timer = 0;
-            UpdateTimerDisplay(timer);
-        }
-
-        if (!countDown && timer != timerDuration)
-        {
-            timer = 0;
-            UpdateTimerDisplay(timer);
-        }
-
-        if ( flashTimer <= 0)
-        {
-            flashTimer = flashDuration;
-        }
-        else if(flashTimer >= flashDuration / 2)
-        {
-            flashTimer -= Time.deltaTime;
-            SetTextDisplay(false);
-        }
-        else
-        {
-            flashTimer -= Time.deltaTime;
-            SetTextDisplay(true);
-        }
-    }
-
-    private void SetTextDisplay(bool enabled)
-    {
-        firstMinute.enabled = enabled;
-        secondMinute.enabled = enabled;
-        separator.enabled = enabled;
-        firstSecond.enabled = enabled;
-        secondSecond.enabled = enabled;
     }
 }
