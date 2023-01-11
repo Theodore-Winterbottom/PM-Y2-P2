c9d7e033d4a35d4283c0b80313f6f2ba0f903066
nikkimartin9555 <96130076+nikkimartin9555@users.noreply.github.com>
2023-01-11 12:40:15 -0500

    timer version 1 done

--- Year-2-P2/Assets/Scripts/Stats.cs
+++ Year-2-P2/Assets/Scripts/Stats.cs
@@ -1,14 +1,18 @@
 using UnityEngine;
 using TMPro;
 using System.Net.NetworkInformation;
+using Unity.VisualScripting;
 
 public class Stats : MonoBehaviour
 {
-    private float timerDuration = 1f * 60f;
+    private float timerDuration = 0.01f * 60f;
+
+    [SerializeField]
+    private bool countDown = true;
 
     private float timer;
-    
 
+    
     [SerializeField]
     private TextMeshProUGUI firstMinute;
     [SerializeField]
@@ -20,28 +24,56 @@ public class Stats : MonoBehaviour
     [SerializeField]
     private TextMeshProUGUI secondSecond;
 
+    private float flashTimer;
+    private float flashDuration = 1f;
+
     // Start is called before the first frame update
     void Start()
     {
-
+        ResetTimer();
     }
 
     // Update is called once per frame
     void Update()
     {
-        // Adds time
-        if(timer > 0)
+        
+        if(countDown && timer > 0)
+        {
+            //Adds time
+            timer += Time.deltaTime;
+            UpdateTimerDisplay(timer);
+        }
+        /*
+        else if (!countDown && timer < timerDuration)
         {
             timer += Time.deltaTime;
             UpdateTimerDisplay(timer);
         }
-      
+        else
+        {
+            Flash();
+        }*/
+    }
+
+    private void ResetTimer()
+    {
+        if(countDown)
+        {
+            timer = timerDuration;
+        }
+        else
+        {
+            timer = 0;
+        }
+        SetTextDisplay(true);
+
+        timer = timerDuration;
     }
 
     private void UpdateTimerDisplay(float time)
     {
-        float minutes = Mathf.FloorToInt(time + 60);
-        float seconds = Mathf.FloorToInt(time + 60);
+        float minutes = Mathf.FloorToInt(time / 60);
+        float seconds = Mathf.FloorToInt(time % 60);
 
         string currentTime = string.Format("{00:00}{1:00}", minutes, seconds);
         firstMinute.text = currentTime[0].ToString();
@@ -50,5 +82,42 @@ public class Stats : MonoBehaviour
         secondSecond.text = currentTime[3].ToString();
     }
 
-    
+    private void Flash()
+    {
+        if(countDown && timer != 0)
+        {
+            timer = 0;
+            UpdateTimerDisplay(timer);
+        }
+
+        if (!countDown && timer != timerDuration)
+        {
+            timer = 0;
+            UpdateTimerDisplay(timer);
+        }
+
+        if ( flashTimer <= 0)
+        {
+            flashTimer = flashDuration;
+        }
+        else if(flashTimer >= flashDuration / 2)
+        {
+            flashTimer -= Time.deltaTime;
+            SetTextDisplay(false);
+        }
+        else
+        {
+            flashTimer -= Time.deltaTime;
+            SetTextDisplay(true);
+        }
+    }
+
+    private void SetTextDisplay(bool enabled)
+    {
+        firstMinute.enabled = enabled;
+        secondMinute.enabled = enabled;
+        separator.enabled = enabled;
+        firstSecond.enabled = enabled;
+        secondSecond.enabled = enabled;
+    }
 }
