69dae6c9a0e815a45e325a5debe6548ec1246f97
DylanNagy <DNAGY.1.2122@tech4maine.org>
2023-01-27 13:25:50 -0500

    some ui

--- Dec.14 New Build/SquirrelKnightAdventures Demo/Assets/Scripts/Scenes/MainMenu.cs
+++ Dec.14 New Build/SquirrelKnightAdventures Demo/Assets/Scripts/Scenes/MainMenu.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -7,9 +8,16 @@ public class MainMenu : MonoBehaviour
 {
 
     [SerializeField] private GameObject SettingsMenu;
+    [SerializeField] private GameObject AudioSettings;
+    [SerializeField] private GameObject VideoSettings;
     public void LoadSettings()
     {
         SettingsMenu.SetActive(true);
+    }
+
+    public void StartButton()
+    {
+        SceneManager.LoadScene(1);
     }
     public void Quit()
     {
@@ -20,5 +28,25 @@ public class MainMenu : MonoBehaviour
     public void BcakButton()
     {
         SettingsMenu.SetActive(false);
+    }
+
+    public void AudioExit()
+    {
+        AudioSettings.SetActive(false);
+    }
+
+    public void VideoExit()
+    {
+        VideoSettings.SetActive(false);
+    }
+
+    public void Audio()
+    {
+        AudioSettings.SetActive(true);
+    }
+
+    public void Video()
+    {
+        VideoSettings.SetActive(true);
     }
 }
