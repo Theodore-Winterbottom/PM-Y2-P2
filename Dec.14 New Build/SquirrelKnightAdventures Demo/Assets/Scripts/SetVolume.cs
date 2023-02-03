a999fb06fee90a6e5e843a4ede8b1032d13e88d3
DylanNagy <DNAGY.1.2122@tech4maine.org>
2023-02-03 13:29:40 -0500

    Music volume, mute, and music select done

--- Dec.14 New Build/SquirrelKnightAdventures Demo/Assets/Scripts/SetVolume.cs
+++ Dec.14 New Build/SquirrelKnightAdventures Demo/Assets/Scripts/SetVolume.cs
@@ -6,12 +6,19 @@ using UnityEngine.Audio;
 public class SetVolume : MonoBehaviour
 {
     public AudioSource audioSource;
+    public AudioSource audioSource2;
+    public AudioSource audioSource3;
+
+    public List<AudioSource> audioSources;
+
 
     private float musicVolume = 1f;
 
     public void Update()
     {
         audioSource.volume = musicVolume;
+        audioSource2.volume = musicVolume;
+        audioSource3.volume = musicVolume;
     }
 
     public void Sound(float volume)
@@ -28,6 +35,31 @@ public class SetVolume : MonoBehaviour
         else
         {
             musicVolume= 1f;
+        }
+    }
+
+    public void DropDown(int value)
+    {
+
+        if (value == 0)
+        {
+            audioSource.Play();
+            audioSource2.Stop();
+            audioSource3.Stop();
+        }
+
+        if (value == 1)
+        {
+            audioSource2.Play();
+            audioSource.Stop();
+            audioSource3.Stop();
+        }
+
+        if (value == 2)
+        {
+            audioSource3.Play();
+            audioSource.Stop();
+            audioSource2.Stop();
         }
     }
 }
